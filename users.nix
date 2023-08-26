7804c54382c7420f82fdae9fef93dee11ace97e6
Álvaro Castillo <31448043+sincorchetes@users.noreply.github.com>
2023-08-26 03:19:10 +0100

    Update users.nix

--- users.nix
+++ users.nix
@@ -6,31 +6,4 @@
     home = "/home/sincorchetes";
     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
   };
-  
-  home-manager.users.sincorchetes = { pkgs, ... }: {
-  programs.bash.enable = true;
-  home.stateVersion = "23.05";
-  nixpkgs.config = {
-    allowUnfree = true;
-  };
-  home.packages = [
-       pkgs.vim
-       pkgs.git
-       pkgs.tree
-       pkgs.htop
-       pkgs.google-chrome
-       pkgs.firefox
-       pkgs.vscode
-       pkgs.teams
-       pkgs.telegram-desktop
-       pkgs.obs-studio
-       pkgs.vlc
-       pkgs.gimp
-       pkgs.inkscape
-       pkgs.tmux
-       pkgs.jetbrains.datagrip
-       pkgs.jetbrains.pycharm-professional
-       pkgs.spotify
-    ];
-  };
 }
