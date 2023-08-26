d3cfbd265cf467e7fda55425302bb78504e26a0f
Álvaro Castillo <31448043+sincorchetes@users.noreply.github.com>
2023-08-26 03:06:27 +0100

    Update boot.nix
    Removes ZFS, hibernation could be corrupt data. https://www.reddit.com/r/zfs/comments/pmcuw1/to_what_extent_does_hibernation_on_zfs_on_linux/

--- boot.nix
+++ boot.nix
@@ -1,19 +1,10 @@
 { config, pkgs, ... }:
 {
-  boot.kernelModules = [ "dm-crypt" "zfs" ];
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot";
-  boot.supportedFilesystems = [ "zfs" ];
-  boot.zfs.requestEncryptionCredentials = true;
-  boot.initrd.luks.devices = {
-      zfsroot = {
-        device = "/dev/disk/by-uuid/ce4b94aa-fd4a-46a4-aa5e-5097e4b978ab";
-        preLVM = true;
-      };
+ boot.loader.systemd-boot.enable = true;
+ boot.loader.efi.canTouchEfiVariables = true;
+ boot.loader.efi.efiSysMountPoint = "/boot";
+ boot.initrd.luks.devices.cryptlvm = {
+  device = "/dev/disk/by-uuid/$(blkid -s UUID -o value /dev/nvme0n1p2)";
+  allowDiscards = true;
   };
-  networking.hostId = "8d761eca";
-  environment.systemPackages = with pkgs; [
-    zfs
-  ];
 }
